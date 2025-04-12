8d7ed550e4fceb0f2c0b2bb253703fa38b59331a
Ravi's World <168616862+Ravis-World@users.noreply.github.com>
2025-04-12 14:28:12 +1000

    Update script.js

--- Heartbeat-Issue-2-Chapter-4/killswitch/1/2/script.js
+++ Heartbeat-Issue-2-Chapter-4/killswitch/1/2/script.js
@@ -1,10 +1,7 @@
 function checkAnswer() {
-    const answer = document.getElementById("answer").value.trim();
-    const correctAnswer = "8^(1/8)"; // The correct answer as a string
-
-    // Compare the user's answer with the correct answer as text
-    if (answer === correctAnswer) {
-        window.location.href = "3/readme.html";
+    const answer = document.getElementById("answer").value;
+    if (answer === "8^(1/8)") {
+        window.location.href = "./3/readme.html";
     } else {
         alert("Incorrect answer. Try again!");
     }
