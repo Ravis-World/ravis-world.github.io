543f8719a652b27b9c5bcf8c00257ccfb9676c02
Ravi's World <168616862+Ravis-World@users.noreply.github.com>
2026-06-16 21:02:30 +1000

    Add ADOFAI items; adjust image fallback & style
    Add multiple A Dance of Fire and Ice community items (item-adofai-1 through item-adofai-7) to the items array with titles, game, descriptions, links, and thumbnail images; fix a duplicated/misaligned ID and link for existing entries. Update thumbnail onerror fallback to a simplified Unsplash URL and change card image styling: object-fit from cover to contain and add a dark background-color (#0c111a) to improve letterboxed thumbnails.

--- Steam/script.js
+++ Steam/script.js
@@ -2,6 +2,14 @@
 const items = [
     {
         id: "item-adofai-1",
+        title: "Hyphen - ^/7(L|?[_(L+#+&|^(o)",
+        game: "A Dance of Fire and Ice",
+        desc: `Head icons created by Pixel perfect from Flaticon: https://www.flaticon.com/free-icons/head\n\nLetters floating around track provided by PikWizard: https://pikwizard.com/png/abstract-scattered-letters-and-symbols-on-transparent-background/4df6ffab6839cca219d8621ef520f471`,
+        link: "https://steamcommunity.com/sharedfiles/filedetails/?id=3731966887",
+        img: "https://images.steamusercontent.com/ugc/18303061561911961812/B1F191EA481470A77648D32396BB3F42DCC8884B/?imw=200&imh=200&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true"
+    },
+    {
+        id: "item-adofai-2",
         title: "CS4W - Collapse",
         game: "A Dance of Fire and Ice",
         desc: `Image of Space by Hans from Pixabay: https://pixabay.com/users/hans-2/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=1655503\n\nWitches Hat Icon by jemastock on Magnific: https://www.magnific.com/free-vector/halloween-witch-hat-isolated-illustration_88414595.htm#fromView=keyword&page=1&position=0&uuid=a6f1fa2d-a6e4-48b3-9a15-1fe041a8531f&query=Witch+hat+png`,
@@ -9,12 +17,44 @@ const items = [
         img: "https://images.steamusercontent.com/ugc/14605721005713364077/4AA3204AC57A7AAC6C4789729A163CE531FB2F5D/?imw=200&imh=200&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true"
     },
     {
-        id: "item-adofai-2",
-        title: "Hyphen - ^/7(L|?[_(L+#+&|^(o)",
+        id: "item-adofai-3",
+        title: "NoLongerNull - Beneath The Final Light",
         game: "A Dance of Fire and Ice",
-        desc: `Head icons created by Pixel perfect from Flaticon: https://www.flaticon.com/free-icons/head\n\nLetters floating around track provided by PikWizard: https://pikwizard.com/png/abstract-scattered-letters-and-symbols-on-transparent-background/4df6ffab6839cca219d8621ef520f471`,
-        link: "hhttps://steamcommunity.com/sharedfiles/filedetails/?id=3731966887",
-        img: "https://images.steamusercontent.com/ugc/18303061561911961812/B1F191EA481470A77648D32396BB3F42DCC8884B/?imw=200&imh=200&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true"
+        desc: `Level Tile Image by juicy_fish on Magnific: https://www.magnific.com/free-vector/performance-meter-cog_392949425.htm#fromView=keyword&page=1&position=0&uuid=d4d7eac3-4905-487c-b269-54cc2b336d72&query=Pressure+logo`,
+        link: "https://steamcommunity.com/sharedfiles/filedetails/?id=3745315129",
+        img: "https://images.steamusercontent.com/ugc/12312989070797195224/3387CF9F5CBA26D2170670AB7C6871925B571873/?imw=200&imh=200&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true"
+    },
+    {
+        id: "item-adofai-4",
+        title: "HyuN - Cross†Revolution (feat. LyuU)",
+        game: "A Dance of Fire and Ice",
+        desc: `Blue Flower Image by gstudioimagen on Freepik: https://www.freepik.com/free-vector/flower-cute-illustration_145244016.htm`,
+        link: "https://steamcommunity.com/sharedfiles/filedetails/?id=3745317565",
+        img: "https://images.steamusercontent.com/ugc/12655133960742327800/04088BE7C318591F7D22D9B8CDA074B48344CC5A/?imw=200&imh=200&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true"
+    },
+    {
+        id: "item-adofai-5",
+        title: "Human Servant - Gamol Hundgēar",
+        game: "A Dance of Fire and Ice",
+        desc: `Glitched Screen Image by Techfiesta - Own work, CC BY-SA 4.0, https://commons.wikimedia.org/w/index.php?curid=66388215\nRoad Video by Kmeel.com from Pixabay: https://pixabay.com/users/kmeel_com-5075826/?utm_source=link-attribution&utm_medium=referral&utm_campaign=video&utm_content=80398\nBlack To White Video by Christian Bodhi from Pixabay: https://pixabay.com/users/christianbodhi-9869182/?utm_source=link-attribution&utm_medium=referral&utm_campaign=video&utm_content=187666\nTunnel Video by Adis Resic from Pixabay: https://pixabay.com/users/adisresic-9188734/?utm_source=link-attribution&utm_medium=referral&utm_campaign=video&utm_content=227152\nEntire Video strung together by me\nLevel Tile icons created by Freepik - Flaticon: https://www.flaticon.com/free-icons/values`,
+        link: "https://steamcommunity.com/sharedfiles/filedetails/?id=3745323899",
+        img: "https://images.steamusercontent.com/ugc/11547719491396603532/B50B1AC86CA5582A312E6AEECEBFF67EF3C94D8F/?imw=200&imh=200&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true"
+    },
+    {
+        id: "item-adofai-6",
+        title: "Sad Keyboard Guy - Lullaby For An Android",
+        game: "A Dance of Fire and Ice",
+        desc: `Background designed by ITV`,
+        link: "https://steamcommunity.com/sharedfiles/filedetails/?id=3745326477",
+        img: "https://images.steamusercontent.com/ugc/11077994790094549120/EDDBBA4CBAAC466ADD36DB996D5EE3E246FAFE12/?imw=200&imh=200&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true"
+    },
+    {
+        id: "item-adofai-7",
+        title: "816ThreeNumbers - Only 1 Minute",
+        game: "A Dance of Fire and Ice",
+        desc: `Level Tile icons created by Freepik - Flaticon: https://www.flaticon.com/free-icons/planet`,
+        link: "https://steamcommunity.com/sharedfiles/filedetails/?id=3745330393",
+        img: "https://images.steamusercontent.com/ugc/12882458856226622362/558FA655CAB26A9FD7D8423448230AAB1271C466/?imw=200&imh=200&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true"
     },
     {
         id: "item-plague-1",
@@ -138,7 +178,7 @@ function filterItems() {
                 <div class="thumbnail-container">
                     <img src="${item.img}" alt="${item.title}" 
                          class="item-thumbnail"
-                         onerror="this.src='https://images.unsplash.com/photo-1612287230202-1bf1d85d1bdf?auto=format&fit=crop&w=600&q=80'">
+                         onerror="this.src='https://images.unsplash.com/photo-1612287230202-1bf1d85d1bdf'">
                 </div>
 
                 <!-- Card Body Content -->
